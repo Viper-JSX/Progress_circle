0ef9e5122d1bdbd6e684a0e0d17608e5e4290a12
Yura_viper <yuramcpecs@gmail.com>
2022-09-18 21:05:55 +0200

    Implemented progressCircle type switching using switch operator

--- src/script.js
+++ src/script.js
@@ -18,6 +18,21 @@ function handleProgressToggle(){
 
 function handleProgressCircleTypeChange(event){
     progressCircleType = event.target.value;
+
+    switch(progressCircleType){
+        case "single-side":{
+            progressFillStrip.style.cssText = "height: 50%; transform-origin: 50% bottom";   
+            break;
+        }
+        case "double-side":{
+            progressFillStrip.style.cssText = "height: 100%; transform-origin: 50% 50%";   
+            break;
+        }
+        default:{
+            progressFillStrip.style.cssText = "height: 50%; transform-origin: 50% bottom";   
+            break
+        }
+    }
 }
 
 toggleProgress.addEventListener("click", handleProgressToggle);
